db6d0adab4401c716a0f9554f03d4a42494d69ae
rkas123 <rkrupeshkumar345@gmail.com>
2022-10-09 16:38:14 +0530

    Add auto login and auto logout.

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,5 +1,9 @@
 import 'dart:convert';
+import 'dart:async';
+
 import '../models/http_exception.dart';
+
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
@@ -9,6 +13,9 @@ class Auth with ChangeNotifier {
   DateTime? _expiryDate;
   String _userId = "";
 
+  Timer? _authTimer = null;
+
+  //requests for token
   bool get isAuth {
     return token != null;
   }
@@ -17,12 +24,21 @@ class Auth with ChangeNotifier {
     return _userId;
   }
 
+  //this function returns token
+  //first we see the auth provider as token or not
+  //if not, we check the token is present in sharedPreferences or not ,i.e, device storage
   String? get token {
     if (_expiryDate != null &&
         _expiryDate!.isAfter(DateTime.now()) &&
         _token != null) {
       return _token as String;
     }
+
+    tryAutoLogin().then((value) {
+      if (value == true) {
+        return _token;
+      }
+    });
     return null;
   }
 
@@ -57,14 +73,6 @@ class Auth with ChangeNotifier {
   Future<void> login(String email, String password) async {
     final url = Uri.parse(
         'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBdJYLswsqQ_muRv0Fxpx384Lo0zc1XeyI');
-    final response = await http.post(
-      url,
-      body: json.encode({
-        'email': email,
-        'password': password,
-        'returnSecureToken': true,
-      }),
-    );
     try {
       final response = await http.post(
         url,
@@ -78,23 +86,68 @@ class Auth with ChangeNotifier {
       if (responseData['error'] != null) {
         throw HttpException(message: responseData['error']['message']);
       }
+
       _token = responseData['idToken'];
       _userId = responseData['localId'];
       _expiryDate = DateTime.now()
           .add(Duration(seconds: int.parse(responseData['expiresIn'])));
+      _autoLogout();
 
       notifyListeners();
+      //shared_preferences code
+      final prefs = await SharedPreferences.getInstance();
+      //prefs now can be used to read and write data
+
+      final userData = json.encode({
+        'token': _token,
+        'userId': _userId,
+        'expiryDate': _expiryDate!.toIso8601String(),
+      });
+
+      prefs.setString('userData', userData);
     } catch (error) {
       rethrow;
     }
   }
 
-  void logout() {
+  Future<bool> tryAutoLogin() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (!prefs.containsKey('userData')) {
+      return false;
+    }
+    final extractedUserData =
+        json.decode(prefs.getString('userData') as String);
+    final expiryDate = DateTime.parse(extractedUserData['expiryDate']);
+    if (expiryDate.isBefore(DateTime.now())) {
+      return false;
+    }
+    //auto Login the user as the token is valid
+    _token = extractedUserData['token'];
+    _userId = extractedUserData['userId'];
+    _expiryDate = expiryDate;
+    notifyListeners();
+    _autoLogout();
+    return true;
+  }
+
+  Future<void> logout() async {
     _token = null;
     _userId = "";
     _expiryDate = null;
+    _authTimer!.cancel;
+
+    final prefs = await SharedPreferences.getInstance();
+    prefs.remove('userData');
     notifyListeners();
   }
 
-  void _autoLogout() {}
+  void _autoLogout() {
+    if (_authTimer != null) {
+      _authTimer!.cancel();
+    }
+    final timeToExpiry = _expiryDate!.difference(DateTime.now()).inSeconds;
+    _authTimer = Timer(Duration(seconds: timeToExpiry), () {
+      logout();
+    });
+  }
 }
